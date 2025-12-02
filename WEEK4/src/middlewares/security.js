f04918969cc50058ae0250d1175355e07ba5ada7
Chandramohan <Senchandramohan2018@gmail.com>
2025-12-02 12:48:01 +0530

    added custom api sanitization function  in security to remove wild chracters like $ and .

--- WEEK4/src/middlewares/security.js
+++ WEEK4/src/middlewares/security.js
@@ -11,10 +11,30 @@ const apiLimiter = rateLimit({
   message: { success: false, message: 'Too many requests' }
 });
 
+const sanitizeInput = (req, res, next) => {
+  const clean = (obj) => {
+    if (!obj || typeof obj !== 'object') return;
+    for (const key in obj) {
+      // Remove keys starting with $ or containing .
+      if (key.startsWith('$') || key.includes('.')) {
+        delete obj[key];
+      } else if (typeof obj[key] === 'object') {
+        clean(obj[key]);  // Recursive clean
+      }
+    }
+  };
+  
+  clean(req.body);
+  clean(req.query);
+  clean(req.params);
+  next();
+};
+
 // Setup function
 function setupSecurity(app) {
   app.use(helmet()); // Security headers
   //app.use(mongoSanitize()); // Remove $ and . from input
+  app.use(sanitizeInput); 
   app.use(express.json({ limit: '10kb' }));
   if (process.env.NODE_ENV === 'production') {
     app.use(cors({ origin: "my-fd-url", credentials: true }));
